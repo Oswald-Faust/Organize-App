c484d0af47e85bc73fefea7448de81c2b54def03
faust.troukou@epitech.eu <ofaust>
2023-08-29 15:43:08 +0100

    Setup application

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Organize'),
       ),
+      body: Center(child: Text('Organize Laucher')),
     );
   }
 }
